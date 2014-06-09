b3cf61cf538e303f4ee335da1d22c91df795cf56
Cynthia Sun <cynthiasun.00@gmail.com>
2014-06-09 02:10:52 -0700

    Trying to get devise on heroku

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -254,6 +254,4 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
-  # Making this work on Heroku
-  config.secret_key = 'cs-omr'
 end
